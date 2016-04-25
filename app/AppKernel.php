36dd7fded56cb143744ae06de7a92d38171f935a
hamouche kosseyla <kosseyla.hamouche@gmail.com>
2016-04-25 14:42:01 +0200

    add softdeletable bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new fredi\AppBundle\frediAppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
